e1ada2ac8ba26650ed65e772d7e893e08344a3e0
Aryan <work.aryan.rec@gmail.com>
2025-01-24 17:05:44 +0530

    Displaying Platform Icons

--- src/Components/GamesCard.tsx
+++ src/Components/GamesCard.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+
 import { Game } from '../hooks/useGames'
-import { Card, Heading, Image, CardBody } from '@chakra-ui/react'
+import { Card, Heading, Image, CardBody, Text } from '@chakra-ui/react'
+import PlatformIconList from './PlatformIconList'
 
 interface Props{
   game: Game
@@ -12,6 +13,7 @@ const GamesCard = ({game}: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize='2xl' >{game.name}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>  
       </CardBody>
     </Card>
   )
