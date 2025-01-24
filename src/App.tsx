97feb3a5acffca615d3bd611fcc2dedc0c757fe4
Aryan <work.aryan.rec@gmail.com>
2025-01-25 00:52:11 +0530

    Displayed Genres

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,26 @@ import GenreList from "./Components/GenreList";
 
 function App() {
   return (
-    <Grid templateAreas={{
+    <Grid
+      templateAreas={{
         base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`
-    }}>
-      <GridItem area="nav" >
-        <NavBar/>
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{ 
+        base: "1fr",
+        lg: '200px 1fr' 
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" >
-          <GenreList/>
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
         </GridItem>
       </Show>
-      <GridItem area="main" >
-        <GameGrid/>
+      <GridItem area="main">
+        <GameGrid />
       </GridItem>
     </Grid>
   );
