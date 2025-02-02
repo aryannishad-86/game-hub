ad15864fd1c78c2234152f2d26ad89c608740cb5
Aryan <work.aryan.rec@gmail.com>
2025-02-02 18:52:46 +0530

    Addes CORS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require ('cors');
+
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
